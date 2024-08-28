5b2acc707033234ce1f96a654208bb7b29eaed95
Frederic MAMATH <fredericmamath@gmail.com>
2024-08-28 22:46:31 +0200

    :construction: Sets minimum configuration to install the extension

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        content: "src/content.tsx",
+        background: "src/background.ts",
+      },
+      output: {
+        entryFileNames: "[name].js",
+      },
+    },
+    outDir: "dist",
+  },
+});
